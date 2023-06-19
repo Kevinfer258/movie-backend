8323b15844e0a418b07881169a7cd639f3c099e2
Kevinfer258 <kevincamilo.ferrerrodelo@gmail.com>
2023-06-19 01:20:55 -0500

    movie-back

---
+++ src/routes/movie.router.js
@@ -0,0 +1,17 @@
+const { getAll, create, getOne, remove, update, setGenre, setDirector, setActors } = require('../controllers/movie.controllers');
+const express = require('express');
+const routerMovie = express.Router();
+routerMovie.route('/')
+    .get(getAll)
+    .post(create);
+routerMovie.route('/:id')
+    .get(getOne)
+    .delete(remove)
+    .put(update);
+routerMovie.route('/:id/genres')
+    .post(setGenre)
+routerMovie.route('/:id/directors')
+    .post(setDirector)
+routerMovie.route('/:id/actors')
+    .post(setActors)
+module.exports = routerMovie;
